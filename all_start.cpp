1424d5af7dfd65ad42d33287390cd5587772d7f4
Joshua-Benfell <31882036+Joshua-Benfell@users.noreply.github.com>
2017-10-27 10:49:49 +1300

    Add files via upload

---
+++ all_start.cpp
@@ -0,0 +1,132 @@
+{
+	{bb[5] , qn},//1st bar
+	{f[5]  , en},//2nd bar start
+	{d[5]  , en},
+	{d[5]  , qn},
+	{c[5]  , en},
+	{bb[4] , en},
+	{bb[4] , en},
+	{eb[5] , en},//2nd bar end
+	{eb[5] , en},//3rd bar start
+	{d[5]  , en},
+	{d[5]  , en},
+	{c[5]  , en},
+	{c[5]  , en},
+	{bb[4] , en},
+	{r     , en},
+	{bb[4] , en},//3rd bar end
+	{f[5]  , en},//4th bar start
+	{d[5]  , en},
+	{d[5]  , en},
+	{c[5]  , en},
+	{c[5]  , en},
+	{bb[4] , en},
+	{bb[4] , en},
+	{g[4]  , en},//4th bar end
+	{g[4]  , hn},//5th bar start
+	{r     , qn},
+	{bb[4] , en},
+	{bb[4] , en},//5th bar end
+	{f[5]  , en},//6th bar start
+	{d[5]  , en},
+	{d[5]  , en},
+	{c[5]  , en},
+	{c[5]  , en},
+	{bb[4] , en},
+	{bb[4] , en},
+	{eb[5] , en},//6th bar end
+	{eb[5] , en},//7th bar start
+	{d[5]  , en},
+	{d[5]  , en},
+	{c[5]  , en},
+	{c[5]  , en},
+	{bb[4] , en},
+	{bb[4] , qn},//7th bar end
+	{f[5]  , en},//8th bar start
+	{d[5]  , en},
+	{d[5]  , en},
+	{c[5]  , en},
+	{c[5]  , en},
+	{bb[4] , qn},
+	{bb[4] , qn},
+	{c[5]  , en},//8th bar end
+	{c[5]  , en+hn},//9th bar start
+	{r     , qn},
+	{bb[4] , en},
+	{g[4]  , en},//9th bar end
+	{bb[4] , en},//10th bar start
+	{bb[4] , en},
+	{bb[4] , sn},
+	{bb[4] , sn},
+	{bb[4] , sn},
+	{bb[4] , sn},
+	{bb[4] , en},
+	{bb[4] , en},
+	{bb[4] , en},
+	{bb[4] , en},//10th bar end
+	{bb[4] , en},//11th bar start
+	{bb[4] , sn},
+	{bb[4] , sn},
+	{bb[4] , en},
+	{bb[4] , sn},
+	{bb[4] , sn},
+	{bb[4] , sn},
+	{bb[4] , sn},
+	{bb[4] , en},
+	{d[5]  , en},
+	{bb[4] , en},//11th bar end
+	{d[5]  , sn},//12th bar start
+	{d[5]  , sn},
+	{f[5]  , en},
+	{eb[5] , en},
+	{d[5]  , sn},
+	{c[5]  , sn},
+	{c[5]  , en},
+	{c[5]  , en},
+	{c[5]  , en},
+	{b[5]  , en},//12th bar end
+	{d[5]  , en},//13th bar start
+	{bb[4] , en},
+	{bb[4] , en},
+	{f[4]  , sn},
+	{f[4]  , sn},
+	{bb[4] , en},
+	{bb[4] , en},
+	{g[4]  , en},
+	{f[4]  , en},//13th bar end
+	{d[5]  , sn},//14th bar start
+	{f[5]  , en},
+	{d[5]  , sn},
+	{g[5]  , en},
+	{d[5]  , sn},
+	{f[5]  , sn},
+	{f[5]  , sn},
+	{d[5]  , sn},
+	{g[5]  , en},
+	{d[5]  , en},
+	{f[5]  , en},//14th bar end
+	{eb[5] , en},//15th bar start
+	{d[5]  , en},
+	{c[5]  , en},
+	{bb[4] , en},
+	{c[5]  , en},
+	{bb[4] , en},
+	{r     , en},
+	{f[4]  , en},//15th bar end
+	{bb[4] , sn},//16th bar start
+	{bb[4] , sn},
+	{bb[4] , en},
+	{bb[4] , sn},
+	{bb[4] , sn},
+	{bb[4] , en},
+	{bb[4] , qn},
+	{r     , en},
+	{f[4]  , en},//16th bar end
+	{d[5]  , sn},//17th bar start
+	{bb[4] , sn},
+	{bb[4] , en},
+	{bb[4] , sn},
+	{bb[4] , sn},
+	{g[5]  , en},
+	{g[5]  , qn},
+	{r     , qn},//17th bar end
